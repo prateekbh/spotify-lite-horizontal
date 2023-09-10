789a13780db5cfd72c06875376e57d7728c127f8
Prateek Bhatnagar <prateek89born@gmail.com>
2023-09-10 18:18:12 +0000

    adding an href

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 
 import styles from './page.module.css'
+import link from "next/link";
 
 export default function Home() {
   return (
     <div>
       hello
+      <div>
+        <link href='/player'> PLayer</link>
+      </div>
     </div>
   )
 }
