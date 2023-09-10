cde2b8b53b222b41b45744e4a9f639461f903fef
Prateek Bhatnagar <prateek89born@gmail.com>
2023-09-10 18:39:58 +0000

    fixing controls

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -24,18 +24,20 @@ export default function Player() {
             <img src="https://i.scdn.co/image/ab67618600001016290e69da72d0ea68cd91c24d" className={style.artistImage} />
         </div>
         <div className={style.dock}>
-            <div className={style.thumbnail}>
-                <img src={playerState.item.album.images.find(img => img.height === 640)?.url} className={style.albumart} width={200} height={200} />
+            <div className={style.info}>
+                <div className={style.thumbnail}>
+                    <img src={playerState.item.album.images.find(img => img.height === 640)?.url} className={style.albumart} width={200} height={200} />
+                </div>
+                <div className={style.trackInfo}>
+                    <div className={style.trackName}>{playerState.item.name}</div>
+                    <div className={style.artistName}>{playerState.item.artists.map(artist=> artist.name).join(", ")}</div>
+                </div>
             </div>
-            <div className={style.trackInfo}>
-                <div className={style.trackName}>{playerState.item.name}</div>
-                <div className={style.artistName}>{playerState.item.artists.map(artist=> artist.name).join(", ")}</div>
-            </div>
-            {/* <div className={style.controls}>
+            <div className={style.controls}>
                 <button className={style.prev}><img src={previousIcon.src} width="100%" height="100%"/></button>
                 <button className={style.playpause}><img src={pauseIcon.src} width="100%" height="100%"/></button>
                 <button className={style.next}><img src={nextIcon.src} width="100%" height="100%"/></button>
-            </div> */}
+            </div>
         </div>
     </div>);
 }
