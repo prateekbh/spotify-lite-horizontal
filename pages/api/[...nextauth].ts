6530e707545f6e2f33f640856046febcbfb9248d
Prateek Bhatnagar <prateek89born@gmail.com>
2023-09-11 16:45:34 +0000

    fix api

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import SpotifyProvider from "next-auth/providers/spotify"
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    Providers.Spotify({
+    SpotifyProvider({
       clientId: '608fc13e3bd647eba6f72859b20bea93',
       clientSecret: '8c8954ab9e0f45d4afa0fd0e33a649f6'
     })
